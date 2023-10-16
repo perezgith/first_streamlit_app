0fd2fa71dfd753a806a7f1198f0d3fa7a692c300
perezgith <147942640+perezgith@users.noreply.github.com>
2023-10-15 20:32:03 -0600

    Update streamlit_app.py
    added new item

--- streamlit_app.py
+++ streamlit_app.py
@@ -4,6 +4,7 @@ streamlit.title('My Parents New Healthy Diner')
 
 streamlit.header('Breakfast Menu')
 
-streamlit.text('* Omega 3 & Blueberry Outmeal')
-streamlit.text('* Kale, Spinach And Rocket Smothie')
-streamlit.text('* Hard-Boile Free-Range Egg')
+streamlit.text('🥣 Omega 3 & Blueberry Outmeal')
+streamlit.text('🥗 Kale, Spinach And Rocket Smothie')
+streamlit.text('🐔 Hard-Boile Free-Range Egg')
+streamlit.text('🥑🍞 Avocado Toast')
