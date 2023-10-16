3fbba99fbb005c4b7e1775179eeb5de36a54ca26
perezgith <147942640+perezgith@users.noreply.github.com>
2023-10-15 20:13:05 -0600

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -3,3 +3,7 @@ import streamlit
 streamlit.title('My Parents New Healthy Diner')
 
 streamlit.title('Breakfast Menu')
+
+'* Omega 3 & Blueberry Outmeal'
+'* Kale, Spinach And Rocket Smothie'
+'* Hard-Boile Free-Range Egg'
